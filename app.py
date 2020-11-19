40c990a1de7b6d08059a1068a6da919a850e7916
Candy Capilla <candycapilla@MacBook-Pro-3.local>
2020-11-18 20:06:24 -0500

    corrected app

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start>add start date in YYYY-MM-DD format<br/>"
+        f"/api/v1.0/<start>/<end> add start date and end date in YYYY-MM-DD format"
     )
 
-@app.route("/api/v1.0/names")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
@@ -72,10 +72,6 @@ def stations():
 
     return jsonify(all_stations)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 @app.route("/api/v1.0/tobs")
 def tobs():
     # Create our session (link) from Python to the DB
@@ -86,6 +82,8 @@ def tobs():
     results = session.query(measurement.date, measurement.tobs).\
         filter(measurement.date >= last_year).\
         filter(measurement.station == 'USC00519281').all()
+    
+    session.close()
 
     #convert to dictionary  
     all_tobs = []
@@ -103,47 +101,56 @@ def start(start):
     # Create our session (link) from Python to the DB
     session = Session(engine)
     #query start to current tmin, tavg, and tmax
-    from_start = session.query(measurement.date, func.min(measurement.tobs),\
+    from_start = session.query(measurement.date,\
+        func.min(measurement.tobs),\
         func.avg(measurement.tobs),func.max(measurement.tobs)).\
         filter(measurement.date >= start).\
         group_by(measurement.date).all()
+
+    session.close()
+    
     #create dict
     start_temp = []
-    for date, min, avg, max in from_start:
+    for date, t_min, t_avg, t_max in from_start:
         start_temp_dict = {}
-        start_temp_dict["Date"] = date
-        start_temp_dict["TMIN"] = min
-        start_temp_dict["TAVG"] = avg
-        start_temp_dict["TMAX"] = max
+        start_temp_dict["date"] = date
+        start_temp_dict["min"] = t_min
+        start_temp_dict["avg"] = t_avg
+        start_temp_dict["max"] = t_max
         start_temp.append(start_temp_dict)
-    session.close()
+    
     #jsonify
-    return jsonify(start_temp_dict)
+    return jsonify(start_temp)
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
+    
     #query start date to end date tmin, tavg, and tmax
-    between_dates = session.query(measurement.date, func.min(measurement.tobs),\
+    between_dates = session.query(measurement.date,\
+        func.min(measurement.tobs),\
         func.avg(measurement.tobs),\
         func.max(measurement.tobs)).\
         filter(measurement.date >= start).\
         filter(measurement.date <= end).\
         group_by(measurement.date).all()
+    
+    session.close()
+
     #create dict
     start_end_temp = []
-    for date, min, avg, max in start_end:
+    for date, t_min, t_avg, t_max in between_dates:
         start_end_temp_dict = {}
-        start_end_temp_dict["Date"] = date
-        start_end_temp_dict["TMIN"] = min
-        start_end_temp_dict["TAVG"] = avg
-        start_end_temp_dict["TMAX"] = max
+        start_end_temp_dict["date"] = date
+        start_end_temp_dict["min"] = t_min
+        start_end_temp_dict["avg"] = t_avg
+        start_end_temp_dict["max"] = t_max
         start_end_temp.append(start_end_temp_dict)
-    session.close()
+    
     #jsonify
-    return jsonify(start_end)
+    return jsonify(start_end_temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
